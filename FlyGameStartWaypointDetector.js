d1e49039e68bc94427d66da051f99c4b4d9c0a12
Kalila L <somnilibertas@gmail.com>
2022-02-07 18:31:35 -0500

    Autolint.

--- FlyGameStartWaypointDetector.js
+++ FlyGameStartWaypointDetector.js
@@ -1,53 +1,55 @@
-(function () {         
-    var consoleID;   
-    var avatarName;    
+(function () {
+    var consoleID;
+    var avatarName;
     var props;
-    var myPosition;    
+    var myPosition;
     var consoleFound = false;
     var myName = "";
     var myID;
     var SEARCH_RADIUS = 10000;
-    
+
 
     this.preload = function (entityID) {
         this.entityID = entityID;
-        myID = entityID;     
+        myID = entityID;
         myPosition = Entities.getEntityProperties(this.entityID, 'position').position;
         myName = Entities.getEntityProperties(this.entityID, 'name').name;
-    };         
-   
+    };
+
     function findConsole() {
         var entities = Entities.findEntities(myPosition, SEARCH_RADIUS);
         for (var i in entities) {
-            props = Entities.getEntityProperties(entities[i]);                  
+            props = Entities.getEntityProperties(entities[i]);
             if (props.name === "BasinskyFlygameConsole") {
                 consoleID = props.id;
                 print(JSON.stringify("Found Console"+consoleID));
-                consoleFound =true;                     
+                consoleFound =true;
             }
-        }        
+        }
     }
-    
-    function sendDataToConsole() {        
-        avatarName = MyAvatar.displayName;      
+
+    function sendDataToConsole() {
+        avatarName = MyAvatar.displayName;
         if (consoleID) {
-            Entities.callEntityServerMethod(             
-                consoleID, 
+            Entities.callEntityServerMethod(
+                consoleID,
                 "receiveDataFromWaypoint",
                 [avatarName,MyAvatar.sessionUUID,myName,myID]
             );
             print(JSON.stringify("Try to send ..... " + avatarName + " + " + myName));
-        }    
+        }
     }
 
-    function waypointEntered() {        
-        if (!consoleFound) {           
-            findConsole();        
+
+
+    function waypointEntered() {
+        if (!consoleFound) {
+            findConsole();
         }
-        sendDataToConsole();       
-    
+        sendDataToConsole();
+
     }
-    this.enterEntity = function(entityID) {       
+    this.enterEntity = function(entityID) {
         waypointEntered(entityID);
-    }; 
+    };
 });
